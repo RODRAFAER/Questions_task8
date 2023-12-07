4a63149557ebc1e36c7a8890e19e16477a4888b5
RODRAFAER <ardorinpeace@gmail.com>
2023-12-07 15:29:49 +0300

    Добавлен перевод числа в таблице ASCII. Теперь файл сохраняется размером в 1 байт. Добавлено считывание значения из файла.

--- Questions.cpp
+++ Questions.cpp
@@ -36,59 +36,56 @@ int main() {
 	ofstream out;
 	out.open("questions.txt");
 
-	ofstream bebe;
-	bebe.open("questions1.txt");
-
 	vector <int> answer;
 
 	cout << "Вы любите кошек? (1 - да, 0 - нет)";
 	cin >> num1;
 	num1 = proverka(num1);
 	answer.push_back(num1);
-	out << num1;
+
 
 	cout << "Вас зовут Михаил? (1 - да, 0 - нет)";
 	cin >> num2;
 	num2 = proverka(num2);
 	answer.push_back(num2);
-	out << num2;
+
 
 	cout << "Да? (1 - да, 0 - нет)";
 	cin >> num3;
 	num3 = proverka(num3);
 	answer.push_back(num3);
-	out << num3;
+
 
 	cout << "Отпускаем группу? (1 - да, 0 - да)";
 	cin >> num4;
 	num4 = proverka(num4);
 	answer.push_back(num4);
-	out << num4;
+
 
 	cout << "Никифор успеет сдать все вопросы до конца семестра? (1 - да, 0 - нет)";
 	cin >> num5;
 	num5 = proverka(num5);
 	answer.push_back(num5);
-	out << num5;
+
 
 	cout << "Вам нравится ваше место работы/учёбы? (1 - да, 0 - нет)";
 	cin >> num6;
 	num6 = proverka(num6);
 	answer.push_back(num6);
-	out << num6;
+
 
 	cout << "Вы любите играть в комп. игры? (1 - да, 0 - нет)";
 	cin >> num7;
 	num7 = proverka(num7);
 	answer.push_back(num7);
-	out << num7;
+
 
 	cout << "В конце 4 курса будете отмечать с нами выпуск? (1 - да, 0 - нет)";
 	cin >> num8;
 	num8 = proverka(num8);
 	answer.push_back(num8);
-	out << num8;
 
+	
 	int perevod = 0;
 	int degree = 0;
 
@@ -97,6 +94,19 @@ int main() {
 		degree++;
 	}
 	
+	char ascii = perevod;
+	out << ascii;
+
+	out.close();
+
+	int a = 1;
+	char ascii2[250];
+
+	ifstream ofile("questions.txt");
+	if (ofile.is_open()) {
+		ofile >> ascii2;
+		cout << ascii2;
+	}
 	
 
 
